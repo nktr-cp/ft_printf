571433c6796e01877c88a76610000f5942a07de2
Kentaro Nishioka <knishiok@c1r24s11.42tokyo.jp>
2023-09-29 00:59:11 +0900

    revert previous commit

--- srcs/printf_parse.c
+++ srcs/printf_parse.c
@@ -6,7 +6,7 @@
 /*   By: knishiok <knishiok@student.42.jp>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/24 16:56:38 by knishiok          #+#    #+#             */
-/*   Updated: 2023/09/29 00:35:13 by knishiok         ###   ########.fr       */
+/*   Updated: 2023/09/29 00:46:56 by knishiok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,58 +28,58 @@ t_format	create_new_format(void)
 	return (fmt);
 }
 
-static void	parse_sub2(const char **s, t_format *fmt)
+static void	parse_sub2(const char ***s, t_format *fmt)
 {
-	if (**s == '-')
+	if (***s == '-')
 	{
 		fmt->flags.left_align = true;
 		fmt->flags.zero_padding = false;
 	}
-	else if (**s == '0')
+	else if (***s == '0')
 	{
 		if (!fmt->flags.left_align)
 			fmt->flags.zero_padding = true;
 	}
-	else if (**s >= '1' && **s <= '9')
+	else if (***s >= '1' && ***s <= '9')
 	{
 		fmt->flags.set_width = true;
-		fmt->width = ft_atoi(s);
-		(*s)--;
+		fmt->width = ft_atoi(*s);
+		(**s)--;
 	}
 }
 
-static void	parse_sub(const char **s, t_format *fmt)
+static void	parse_sub(const char ***s, t_format *fmt)
 {
-	if (**s == '-' || **s == '0' || (**s >= '0' && **s <= '9'))
+	if (***s == '-' || ***s == '0' || (***s >= '0' && ***s <= '9'))
 		parse_sub2(s, fmt);
-	else if (**s == ' ')
+	else if (***s == ' ')
 		fmt->flags.space = true;
-	else if (**s == '+')
+	else if (***s == '+')
 		fmt->flags.sign = true;
-	else if (**s == '#')
+	else if (***s == '#')
 		fmt->flags.sharp = true;
-	else if (**s == '.')
+	else if (***s == '.')
 	{
 		fmt->flags.period = true;
-		(*s)++;
-		if ((**s >= '0' && **s <= '9') || ft_strchr(SPECIFIER, **s))
+		(**s)++;
+		if ((***s >= '0' && ***s <= '9') || ft_strchr(SPECIFIER, ***s))
 		{
 			fmt->flags.precision = true;
-			fmt->precision = ft_atoi(s);
-			(*s)--;
+			fmt->precision = ft_atoi(*s);
+			(**s)--;
 		}
 		else
 			fmt->flags.period = false;
 	}
-	(*s)++;
+	(**s)++;
 }
 
-t_format	parse_flags(const char *s)
+t_format	parse_flags(const char **s)
 {
 	t_format	fmt;
 
 	fmt = create_new_format();
-	while (!ft_strchr(SPECIFIER, *s))
+	while (!ft_strchr(SPECIFIER, **s))
 		parse_sub(&s, &fmt);
 	return (fmt);
 }
